245cae50da4773a8b9ec57a36e64b3012c752c88
fraserxu <xvfeng123@gmail.com>
2015-11-13 13:31:03 +0800

    init project

---
+++ android/src/main/java/me/fraserxu/rncouchbaselite/ReactCBLite.java
@@ -0,0 +1,77 @@
+package me.fraserxu.rncouchbaselite
+
+import com.couchbase.lite.Manager;
+import com.couchbase.lite.View;
+import com.couchbase.lite.android.AndroidContext;
+import com.couchbase.lite.javascript.JavaScriptViewCompiler;
+import com.couchbase.lite.listener.Credentials;
+import com.couchbase.lite.listener.LiteListener;
+import com.couchbase.lite.util.Log;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.Callback;
+
+public class ReactCBLite extends ReactContextBaseJavaModule {
+
+    public static final String REACT_CLASS = "ReactCBLite";
+    private static final int DEFAULT_LISTEN_PORT = 5984;
+    private int listenPort;
+    private Credentials allowedCredentials;
+
+    public ReactCBLite(ReactApplicationContext reactContext) {
+        super(reactContext);
+    }
+
+    @Override
+    public String getName() {
+        return REACT_CLASS;
+    }
+
+    @ReactMethod
+    public void init() {
+        initCBLite();
+    }
+
+    private void initCBLite() {
+        try {
+            allowedCredentials = new Credentials("", "");
+
+            View.setCompiler(new JavaScriptViewCompiler());
+
+            AndroidContext context = new AndroidContext(this);
+            Manager.enableLogging(Log.TAG, Log.VERBOSE);
+            Manager.enableLogging(Log.TAG_SYNC, Log.VERBOSE);
+            Manager.enableLogging(Log.TAG_QUERY, Log.VERBOSE);
+            Manager.enableLogging(Log.TAG_VIEW, Log.VERBOSE);
+            Manager.enableLogging(Log.TAG_CHANGE_TRACKER, Log.VERBOSE);
+            Manager.enableLogging(Log.TAG_BLOB_STORE, Log.VERBOSE);
+            Manager.enableLogging(Log.TAG_DATABASE, Log.VERBOSE);
+            Manager.enableLogging(Log.TAG_LISTENER, Log.VERBOSE);
+            Manager.enableLogging(Log.TAG_MULTI_STREAM_WRITER, Log.VERBOSE);
+            Manager.enableLogging(Log.TAG_REMOTE_REQUEST, Log.VERBOSE);
+            Manager.enableLogging(Log.TAG_ROUTER, Log.VERBOSE);
+            Manager manager = new Manager(context, Manager.DEFAULT_OPTIONS);
+
+            listenPort = startCBLListener(DEFAULT_LISTEN_PORT, manager, allowedCredentials);
+
+            Log.i(TAG, "initCBLite() completed successfully with: " + String.format(
+                    "http://%s:%s@localhost:%d/",
+                    allowedCredentials.getLogin(),
+                    allowedCredentials.getPassword(),
+                    listenPort));
+
+        } catch (final Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private int startCBLListener(int listenPort, Manager manager, Credentials allowedCredentials) {
+        LiteListener listener = new LiteListener(manager, listenPort, allowedCredentials);
+        int boundPort = listener.getListenPort();
+        Thread thread = new Thread(listener);
+        thread.start();
+        return boundPort;
+    }
+}
