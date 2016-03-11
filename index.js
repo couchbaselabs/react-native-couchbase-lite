ac445947c09a766aa07b045374ced877ff0c0b4b
Adam Wilson <adam@codegarden.co.uk>
2016-03-11 23:00:53 +0000

    Fix doc sections for syntax highlighting

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ manager.prototype = {
    * @returns {*|promise}
    */
   createDatabase: function() {
-    return this.makeRequest("PUT", this.databaseUrl + this.databaseName, {}, null);
+    return this.makeRequest("PUT", this.databaseUrl + this.databaseName, null, null);
   },
 
   /**
@@ -29,7 +29,7 @@ manager.prototype = {
     return this.makeRequest("DELETE", this.databaseUrl + this.databaseName, null, null);
   },
 
-  /*
+  /**
    * Create a new design document with views
    *
    * @param    string designDocumentName
@@ -40,10 +40,10 @@ manager.prototype = {
     var data = {
       views: designDocumentViews
     };
-    return this.makeRequest("PUT", this.databaseUrl + this.databaseName + "/_design/" + designDocumentName, {}, data);
+    return this.makeRequest("PUT", this.databaseUrl + this.databaseName + "/" + designDocumentName, {}, data);
   },
 
-  /*
+  /**
    * Get a design document and all views associated to insert
    *
    * @param    string designDocumentName
@@ -53,35 +53,16 @@ manager.prototype = {
     return this.makeRequest("GET", this.databaseUrl + this.databaseName + "/" + designDocumentName);
   },
 
-  /*
-   * Query a particular database view. Options for the query ('descending', 'limit', 'startkey', 'endkey' etc.)
-   * can be specified using query string parameters.  Query string values are json objects and are URL encoded within,
-   * for example:
-   *
-   *  let options = {
-   *    descending: true,
-   *    startkey: [docId, {}],
-   *    endkey: [docId]
-   *  };
-   *
-   *  return queryView('design_doc_name', 'view_name', options);
+  /**
+   * Query a particular database view
    *
    * @param    string designDocumentName
    * @param    string viewName
-   * @param    object queryStringParameters
+   * @param    object options
    * @return   promise
    */
-  queryView: function(designDocumentName, viewName, queryStringParameters) {
-    var url = this.databaseUrl + this.databaseName + "/_design/" + designDocumentName + "/_view/" + viewName;
-
-    if(queryStringParameters) {
-      for(var key in queryStringParameters) {
-        var value = queryStringParameters[key];
-        queryStringParameters[key] = JSON.stringify(value);
-      }
-    }
-
-    return this.makeRequest("GET", url, queryStringParameters);
+  queryView: function(designDocumentName, viewName, options) {
+    return this.makeRequest("GET", this.databaseUrl + this.databaseName + "/" + designDocumentName + "/_view/" + viewName, options);
   },
 
   /**
@@ -95,34 +76,6 @@ manager.prototype = {
   },
 
   /**
-   * Add, update, or delete multiple documents to a database in a single request
-   *
-   * @param object jsonDocuments array
-   * @returns {*|promise}
-   */
-  modifyDocuments: function (jsonDocuments) {
-    return this.makeRequest("POST", this.databaseUrl + this.databaseName + '/_bulk_docs', {}, {docs: jsonDocuments});
-  },
-
-
-  /**
-   * Creates a new document or creates a new revision of an existing document
-   *
-   * @param object jsonDocument
-   * @param string documentRevision (optional)
-   * @returns {*|promise}
-   */
-  updateDocument: function (jsonDocument, documentRevision) {
-    var options = {}
-
-    if(documentRevision) {
-        options.rev = documentRevision;
-    }
-
-    return this.makeRequest("PUT", this.databaseUrl + this.databaseName + "/" + jsonDocument._id, options, jsonDocument);
-  },
-
-  /**
    * Delete a particular document based on its id and revision
    *
    * @param documentId
@@ -130,22 +83,17 @@ manager.prototype = {
    * @return promise
    */
   deleteDocument: function(documentId, documentRevision) {
-    return this.makeRequest("DELETE", this.databaseUrl + this.databaseName + "/" + documentId, {rev: documentRevision});
+    return this.makeRequest("DELETE", this.databaseUrl + this.databaseName + "/" + documentId + "?rev=" + documentRevision);
   },
 
-  /*
-   * Get a document with optional revision from the database
+  /**
+   * Get a document from the database
    *
    * @param    string documentId
-   * @param    string revision
    * @return   promise
    */
-  getDocument: function(documentId, rev) {
-    let options = {};
-    if(rev) {
-      options.rev = rev;
-    }
-    return this.makeRequest("GET", this.databaseUrl + this.databaseName + "/" + documentId, options);
+  getDocument: function(documentId) {
+    return this.makeRequest("GET", this.databaseUrl + this.databaseName + "/" + documentId);
   },
 
   /**
@@ -154,50 +102,23 @@ manager.prototype = {
    * @returns {*|promise}
    */
   getAllDocuments: function() {
-    return this.makeRequest("GET", this.databaseUrl + this.databaseName + "/_all_docs", {include_docs: true});
+    return this.makeRequest("GET", this.databaseUrl + this.databaseName + "/_all_docs?include_docs=true");
   },
 
   /**
-   * Get all conflicts
-   *
-   * @returns {*|promise}
-   */
-  getAllDocumentConflicts: function() {
-    return this.makeRequest("GET", this.databaseUrl + this.databaseName + "/_all_docs", {only_conflicts: true});
-  },
-
-  /**
-   * Replicate in a single direction whether that be remote from local or local to remote.
+   * Replicate in a single direction whether that be remote from local or local to remote
    *
    * @param source
    * @param target
    * @param continuous
-   * @param sessionCookie (optional)
    * @returns {*|promise}
    */
-  replicate: function(source, target, continuous, sessionCookie) {
-    var replicateUrl = this.databaseUrl + "_replicate";
-
-    if(sessionCookie) {
-      return this.makeRequest("POST", replicateUrl, {}, {
-        source: source,
-        target: {
-          headers: {
-            Cookie: sessionCookie
-          },
-          url: target
-        },
-        continuous: continuous
-      });
-    } else {
-      return this.makeRequest("POST", replicateUrl, {}, {
-        source: source,
-        target: target,
-        continuous: continuous
-      });
-    }
-
-
+  replicate: function(source, target, continuous) {
+    return this.makeRequest("POST", this.databaseUrl + "_replicate", {}, {
+      source: source,
+      target: target,
+      continuous: continuous
+    });
   },
 
   /**
@@ -209,7 +130,7 @@ manager.prototype = {
    * @param object data
    * @returns {*|promise}
    */
-  makeRequest: function(method, url, queryStringParameters, data) {
+  makeRequest: function(method, url, params, data) {
     var settings = {
       method: method,
       headers: {
@@ -218,39 +139,18 @@ manager.prototype = {
         'Authorization': this.authHeader
       }
     };
-
-    var queryString = "";
-
-    if(queryStringParameters) {
-      var parts = [];
-
-      for(var key in queryStringParameters) {
-        var value = queryStringParameters[key];
-        var part = key + "=" + encodeURIComponent(value);
-        parts.push(part);
-      }
-
-      if(parts.length > 0) {
-          queryString = "?" + parts.join("&");
-      }
+    if (params) {
+      settings.params = params;
     }
-
-    var fullUrl = url + queryString;
-
     if (data) {
       settings.body = JSON.stringify(data);
     }
-
-    return fetch(fullUrl, settings).then((res) => {
+    return fetch(url, settings).then((res) => {
       if (res.status == 401) {
-        console.warn(res);
-
-        throw new Error("Not authorized to access '" + fullUrl + "' [" + res.status + "]");
+        console.log(res);
       }
       return res.json();
-    }).catch((err) => {
-        throw new Error("http error for '" + fullUrl + "', caused by => " + err);
-    });
+    }).catch((err) => { throw err; });
   }
 };
 
