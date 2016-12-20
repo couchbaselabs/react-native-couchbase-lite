60dff69c53dc71d8a7cd9e716ac721ff1a6c15ea
James Nocentini <james.nocentini@gmail.com>
2016-12-20 19:47:38 +0000

    Use base-64 module to add Authorization header and bump swagger-client version

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 import {NativeModules} from "react-native";
 import Swagger from "swagger-client";
 import spec from "./spec.json";
+import base64 from 'base-64';
 const Couchbase = NativeModules.ReactCBLite;
 
 let manager, callback;
@@ -12,6 +13,8 @@ Couchbase.init(url => {
 
   new Swagger({spec: spec, usePromise: true})
     .then(client => {
+      var encodedCredentials = "Basic " + base64.encode(url.split("//")[1].split('@')[0]);
+      client.clientAuthorizations.add("auth", new Swagger.ApiKeyAuthorization('Authorization', encodedCredentials, 'header'));
       manager = client;
       if (typeof callback == 'function') {
         callback(manager);
