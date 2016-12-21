c3a177c7432b2c9991d0a16573be20d06b0cfbe6
James Nocentini <james.nocentini@gmail.com>
2016-12-21 14:41:22 +0000

    Update rnpm-postlink to add Jackson license files to project's build.gradle

--- scripts/rnpm-postlink.js
+++ scripts/rnpm-postlink.js
@@ -4,41 +4,34 @@
 
 var fs = require('fs');
 var rnpm = require('rnpm/src/config');
-var xcode = require('xcode');
 
 // The current working directory should be project root of the app that is linking Realm.
 var config = rnpm.getProjectConfig();
 
-if (config.ios) {
-  var pbxproj = config.ios.pbxprojPath;
-  var project = xcode.project(pbxproj).parseSync();
-  var target = project.getFirstTarget().uuid;
-
-  // Create a Frameworks group if necessary.
-  if (!project.pbxGroupByName('Frameworks')) {
-    var group = project.pbxCreateGroup('Frameworks', '""');
-    var mainGroup = project.getFirstProject().firstProject.mainGroup;
-
-    project.getPBXGroupByKey(mainGroup).children.push({
-      value: group,
-      comment: 'Frameworks',
-    });
-  }
-
-  ['libz', 'libsqlite3'].forEach(function(name) {
-    project.addFramework('usr/lib/' + name + '.tbd', {
-      lastKnownFileType: 'sourcecode.text-based-dylib-definition',
-      sourceTree: 'SDKROOT',
-      target: target,
-    });
+if (config.android) {
+  var projectGradle = 'android/app/build.gradle';
+  fs.readFile(projectGradle, 'utf8', function (err, data) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    var text = "\
+    // Required for Couchbase Lite Android \n\
+    // Workaround for duplicate files during packaging of APK issue \n\
+    // see https://groups.google.com/d/msg/adt-dev/bl5Rc4Szpzg/wC8cylTWuIEJ \n\
+    packagingOptions { \n\
+      exclude 'META-INF/ASL2.0' \n\
+      exclude 'META-INF/LICENSE' \n\
+      exclude 'META-INF/NOTICE' \n\
+    }\n\n";
+    if (data.indexOf(text) == -1) {
+      var position = data.indexOf("android {") + 10;
+      var newData = [data.slice(0, position), text, data.slice(position)].join('');
+      fs.writeFile(projectGradle, newData, function (err) {
+        if (err) {
+          console.log(err);
+        }
+      });
+    }
   });
-
-  ['SystemConfiguration', 'CFNetwork', 'Security'].forEach(function (name) {
-    project.addFramework('/System/Library/Frameworks/' + name + '.framework', {
-      sourceTree: 'SDKROOT',
-      target: target,
-    });
-  });
-
-  fs.writeFileSync(pbxproj, project.writeSync());
 }
